02357ec3a958df0119d7cf2063afd6efabc63748
puku0x <puku0x@gmail.com>
2020-11-27 23:59:28 +0900

    test(todo-create): add snapshot test

---
+++ src/pages/todo/todo-create/components/todo-create/todo-create.component.spec.ts
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils';
+import { createRouter, createMemoryHistory } from 'vue-router';
+
+import { routes } from '@/router';
+
+import TodoCreate from './todo-create.component.vue';
+
+describe('TodoCreate', () => {
+  it('render', async () => {
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes
+    });
+    router.push('/todos/new');
+    await router.isReady();
+    const wrapper = mount(TodoCreate, {
+      global: {
+        plugins: [router]
+      },
+      props: {
+        isFetching: false
+      }
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
