6576e04372a739ae869a44c30e5515de7b9d94bb
puku0x <puku0x@gmail.com>
2020-11-28 00:05:51 +0900

    test(todo-edit): add snapshot test

---
+++ src/pages/todo/todo-edit/components/todo-edit/todo-edit.component.spec.ts
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils';
+import { createRouter, createMemoryHistory } from 'vue-router';
+
+import { Todo } from '@/models';
+import { routes } from '@/router';
+
+import TodoEdit from './todo-edit.component.vue';
+
+describe('TodoEdit', () => {
+  it('render', async () => {
+    const todo: Todo = {
+      id: '1',
+      title: 'title',
+      completed: false,
+      createdAt: 123456789,
+      updatedAt: 123456789
+    };
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes
+    });
+    router.push('/todos/1/edit');
+    await router.isReady();
+    const wrapper = mount(TodoEdit, {
+      global: {
+        plugins: [router]
+      },
+      props: {
+        isFetching: false,
+        todo
+      }
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
