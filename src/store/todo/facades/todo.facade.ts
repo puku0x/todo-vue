cdee0fdd25c7b3d759d871dc04a9848a887488c9
puku0x <puku0x@gmail.com>
2021-01-02 14:40:17 +0900

    refactor(store): refactor facade

--- src/store/todo/facades/todo.facade.ts
+++ src/store/todo/facades/todo.facade.ts
@@ -3,46 +3,45 @@ import { useStore } from 'vuex';
 
 import { Todo, TodoCreateDto, TodoUpdateDto } from '@/models';
 
-import {
-  FETCH_ALL_REQUEST,
-  FETCH_REQUEST,
-  CREATE_REQUEST,
-  UPDATE_REQUEST,
-  REMOVE_REQUEST,
-} from '../actions';
+import * as actions from '../actions';
+import * as getters from '../getters';
 import { featureKey } from '../states';
 
 export const useTodoStore = () => {
   const store = useStore();
   const isFetching = computed<boolean>(
-    () => store.getters[`${featureKey}/isFetching`]
+    () => store.getters[`${featureKey}/${getters.isFetching.name}`]
   );
-  const todos = computed<Todo[]>(() => store.getters[`${featureKey}/todos`]);
-  const todo = computed<Todo | null>(() => store.getters[`${featureKey}/todo`]);
+  const todos = computed<Todo[]>(
+    () => store.getters[`${featureKey}/${getters.todos.name}`]
+  );
+  const todo = computed<Todo | null>(
+    () => store.getters[`${featureKey}/${getters.todo.name}`]
+  );
 
   const fetchAll = (
     arg: { offset?: number; limit?: number } = {}
   ): Promise<{ todos: Todo[] }> => {
-    return store.dispatch(`${featureKey}/${FETCH_ALL_REQUEST}`, arg);
+    return store.dispatch(`${featureKey}/${actions.FETCH_ALL_REQUEST}`, arg);
   };
 
   const fetch = (arg: { id: string }): Promise<{ todo: Todo }> => {
-    return store.dispatch(`${featureKey}/${FETCH_REQUEST}`, arg);
+    return store.dispatch(`${featureKey}/${actions.FETCH_REQUEST}`, arg);
   };
 
   const create = (arg: { todo: TodoCreateDto }): Promise<{ todo: Todo }> => {
-    return store.dispatch(`${featureKey}/${CREATE_REQUEST}`, arg);
+    return store.dispatch(`${featureKey}/${actions.CREATE_REQUEST}`, arg);
   };
 
   const update = (arg: {
     id: string;
     todo: TodoUpdateDto;
   }): Promise<{ todo: Todo }> => {
-    return store.dispatch(`${featureKey}/${UPDATE_REQUEST}`, arg);
+    return store.dispatch(`${featureKey}/${actions.UPDATE_REQUEST}`, arg);
   };
 
   const remove = (arg: { id: string }): Promise<{ id: string }> => {
-    return store.dispatch(`${featureKey}/${REMOVE_REQUEST}`, arg);
+    return store.dispatch(`${featureKey}/${actions.REMOVE_REQUEST}`, arg);
   };
 
   return {
