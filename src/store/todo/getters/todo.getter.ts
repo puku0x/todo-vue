48587fc5f3e6bb58fd6df2d0c6f980b4e3f52db0
puku0x <puku0x@gmail.com>
2021-01-02 13:50:54 +0900

    refactor(store): refactor getter

--- src/store/todo/getters/todo.getter.ts
+++ src/store/todo/getters/todo.getter.ts
@@ -3,13 +3,18 @@ import { GetterTree } from 'vuex';
 import { RootState } from '../../store';
 import { TodoState } from '../states';
 
+export const isFetching = (state: TodoState) => state.isFetching;
+export const selectedId = (state: TodoState) => state.selectedId;
+export const todos = (state: TodoState) => state.todos;
+export const todo = (state: TodoState) => {
+  return state.selectedId
+    ? state.todos.find((todo) => todo.id === state.selectedId) ?? null
+    : null;
+};
+
 export const getters: GetterTree<TodoState, RootState> = {
-  isFetching: (state) => state.isFetching,
-  selectedId: (state) => state.selectedId,
-  todos: (state) => state.todos,
-  todo: (state) => {
-    return state.selectedId
-      ? state.todos.find((todo) => todo.id === state.selectedId) ?? null
-      : null;
-  },
+  isFetching,
+  selectedId,
+  todos,
+  todo,
 };
