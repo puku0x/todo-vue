719aafc4595dc94a39779a7b4116441f637a4bd0
puku0x <puku0x@gmail.com>
2021-01-02 13:52:16 +0900

    refactor(main.ts): fix format

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,4 @@ import { store } from '@/store';
 
 import App from './app.vue';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app');
+createApp(App).use(store).use(router).mount('#app');
